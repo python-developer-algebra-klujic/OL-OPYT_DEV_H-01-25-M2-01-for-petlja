6c3c5e80acb5fcb2ae976eb5409bee9d72e27cb0
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-05 19:20:00 +0100

    Update main_01.py

--- main_01.py
+++ main_01.py
@@ -15,17 +15,19 @@ print('-'*CHARS)
 
 fuel_consumption = float(input('Upisite potrosnju goriva vozila (l/100km): '))
 fuel_price = float(input('Cijena goriva (EUR/l): '))
+commuting_distance_km = int(input('Upisite udaljenost u km do posla u jednom smijeru: '))
 
 price_per_km = (fuel_consumption / 100) * fuel_price
-commuting_distance_km = 20
 daily_distance_km = 2 * commuting_distance_km
 monthly_distance_km = 30 * daily_distance_km
 
+# daily_costs = round(daily_distance_km * price_per_km, 2)
+# monthly_costs = round(monthly_distance_km * price_per_km, 2)
 daily_costs = daily_distance_km * price_per_km
 monthly_costs = monthly_distance_km * price_per_km
 
-display_daily_costs = f'Dnevni trosak odlaska na posao autom iznosi: {daily_costs}.'
-display_monthly_costs = f'Mjesecni trosak odlaska na posao autom iznosi: { monthly_costs}.'
+display_daily_costs = f'Dnevni trosak odlaska na posao autom iznosi: {daily_costs:.2f} EUR.'
+display_monthly_costs = f'Mjesecni trosak odlaska na posao autom iznosi: {monthly_costs:.2f} EUR.'
 
 print()
 print('-'*CHARS)
