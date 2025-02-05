c480c6c93f89885cbfca14d226a6f6332f788e24
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-05 19:52:05 +0100

    Create main_02.py

---
+++ main_02.py
@@ -0,0 +1,62 @@
+# Kreirati program koji ce omoguciti korisniku unos podataka za 10 clanova grupe
+# Podaci koje treba pohraniti su: ime, prezime, godina rodenja i ID clana.
+
+# Clan broj 1
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+# Clan broj 2
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+# Clan broj 3
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+# Clan broj 4
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+# Clan broj 5
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+# Clan broj 6
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+# Clan broj 7
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+# Clan broj 8
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+# Clan broj 9
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+# Clan broj 10
+member_id = int(input('Upisite ID broj clana: '))
+first_name = input('Upisite ime clana: ')
+last_name = input('Upisite prezime clana: ')
+year_of_birth = int(input('Upisite godinu rodenja clana: '))
