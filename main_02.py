1aa2568d94796aa1951462c08e87a6060cf5c6d6
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-05 20:09:32 +0100

    Summary

--- main_02.py
+++ main_02.py
@@ -2,22 +2,22 @@
 # Podaci koje treba pohraniti su: ime, prezime, godina rodenja i ID clana.
 
 # Clan broj 1
-member_id = int(input('Upisite ID broj clana: '))
-first_name = input('Upisite ime clana: ')
-last_name = input('Upisite prezime clana: ')
-year_of_birth = int(input('Upisite godinu rodenja clana: '))
+member_id_1 = int(input('Upisite ID broj 1. clana: '))
+first_name_1 = input('Upisite ime 1. clana: ')
+last_name_1 = input('Upisite prezime 1. clana: ')
+year_of_birth_1 = int(input('Upisite godinu rodenja 1. clana: '))
 
 # Clan broj 2
-member_id = int(input('Upisite ID broj clana: '))
-first_name = input('Upisite ime clana: ')
-last_name = input('Upisite prezime clana: ')
-year_of_birth = int(input('Upisite godinu rodenja clana: '))
+member_id_2 = int(input('Upisite ID broj 2. clana: '))
+first_name_2 = input('Upisite ime 2. clana: ')
+last_name_2 = input('Upisite prezime 2. clana: ')
+year_of_birth_2 = int(input('Upisite godinu rodenja 2. clana: '))
 
 # Clan broj 3
-member_id = int(input('Upisite ID broj clana: '))
-first_name = input('Upisite ime clana: ')
-last_name = input('Upisite prezime clana: ')
-year_of_birth = int(input('Upisite godinu rodenja clana: '))
+member_id_3 = int(input('Upisite ID broj clana: '))
+first_name_3 = input('Upisite ime clana: ')
+last_name_3 = input('Upisite prezime clana: ')
+year_of_birth_3 = int(input('Upisite godinu rodenja clana: '))
 
 # Clan broj 4
 member_id = int(input('Upisite ID broj clana: '))
---
+++ main_03.py
@@ -0,0 +1,12 @@
+# Kreirati program koji ce omoguciti korisniku unos podataka za 10 clanova grupe
+# Podaci koje treba pohraniti su: ime, prezime, godina rodenja i ID clana.
+
+
+for i in range(10):
+    member_id = int(input('Upisite ID broj 1. clana: '))
+    first_name = input('Upisite ime clana: ')
+    last_name = input('Upisite prezime clana: ')
+    year_of_birth = int(input('Upisite godinu rodenja clana: '))
+
+
+print('Kraj programa')
