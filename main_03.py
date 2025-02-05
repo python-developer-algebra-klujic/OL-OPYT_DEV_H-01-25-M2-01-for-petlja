f402244312d79b00edcaca1ece22b633b4528c5f
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-05 20:16:55 +0100

    Update main_03.py

--- main_03.py
+++ main_03.py
@@ -2,11 +2,12 @@
 # Podaci koje treba pohraniti su: ime, prezime, godina rodenja i ID clana.
 
 
-for i in range(10):
-    member_id = int(input('Upisite ID broj 1. clana: '))
-    first_name = input('Upisite ime clana: ')
-    last_name = input('Upisite prezime clana: ')
-    year_of_birth = int(input('Upisite godinu rodenja clana: '))
+for i in range(10): # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 !!! NE I 10 !!!
+    # member_id = int(input(f'Upisite ID broj {i + 1}. clana: '))
+    member_id = i + 1
+    first_name = input(f'Upisite ime {i + 1}. clana: ')
+    last_name = input(f'Upisite prezime {i + 1}. clana: ')
+    year_of_birth = int(input(f'Upisite godinu rodenja {i + 1}. clana: '))
 
 
 print('Kraj programa')
