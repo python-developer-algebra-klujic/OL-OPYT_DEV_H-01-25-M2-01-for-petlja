d2861040c7ebbee4ef24fb877c2f474c58b75a06
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-07 19:01:03 +0100

    Update main_06.py

--- main_06.py
+++ main_06.py
@@ -12,3 +12,43 @@
        **
        **
 '''
+
+
+
+height = int(input('Upisite visinu bozicnog drvca: '))
+symbol = input('Upisite znak koji ce biti iscrtano drvce: ')
+
+# print((height // 2) + 1)
+
+for i in range((height // 2) + 1):
+   print(f' ' * ((height // 2) - i), end='')
+   print(f'{symbol}' * ((i*2) + 1))
+
+for j in range(2):
+   print(f' ' * ((height // 2) - 1), end='')
+   print(f'|' * 3)
+
+
+
+
+
+
+
+
+
+# Rjesenje kolege: Mario Vojić
+
+# razmak=int(input("Unesite visinu drvca: "))
+# print()
+
+# postolje_razmak=razmak-1
+
+
+
+# for i in range(razmak):
+#     print(f' '*(razmak-i)+f'*'*((i*2)+1))
+
+# for j in range(2):
+#     print(f' '*postolje_razmak+f'#'*2)
+
+# print()
